64033594f2c75326cbf6c9c3bc05f4113d7ccd08
Samuel de Lucas <samuel.lucas@estudiante.uam.es>
2023-02-27 12:09:07 +0100

    Update .config/nvim/after/plugin/vimtex.lua

--- dot_config/nvim/after/plugin/vimtex.lua
+++ dot_config/nvim/after/plugin/vimtex.lua
@@ -1,2 +1,7 @@
 vim.cmd('filetype plugin indent on')
 vim.cmd('syntax enable')
+local g = vim.g
+g.vimtex_view_method = 'zathura_simple'
+g.maplocalleader = ","
+-- g.vimtex_syntax_enabled = 0
+-- g.vimtex_syntax_conceal_disable = 1
